5b3cc424b535fd6ff2383b593bb55d1d8cbfb7c4
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2019-05-07 02:17:07 +0300

    Add: recipe model

--- hipo/recipe/models.py
+++ hipo/recipe/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+RECIPE_STAGE_CHOICES = (
+    ('easy', 'Easy'),
+    ('medium', 'Medium'),
+    ('hard', 'Hard'),
+)
+
+
+class Recipe(models.Model):
+    """
+    """
+    recipe_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    recipe_stage = models.CharField(
+        max_length=30,
+        choices=RECIPE_STAGE_CHOICES,
+    )
+    recipe_name = models.CharField(max_length=30)
+    recipe_content = models.CharField(max_length=255)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.recipe_name
